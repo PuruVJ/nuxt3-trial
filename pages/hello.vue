02558add3de56d324b507ec4b932693023ddd279
Puru Vijay <devpuruvj@gmail.com>
2022-04-12 16:02:52 +0530

    Wrrap everything in a div

--- pages/hello.vue
+++ pages/hello.vue
@@ -1,20 +1,22 @@
 <template>
-  <Head>
-    <Title>Hello world</Title>
-  </Head>
+  <div>
+    <Head>
+      <Title>Hello world</Title>
+    </Head>
 
-  <button @click="count++">Increment me</button>
+    <button @click="count++">Increment me</button>
 
-  <h1>{{ count }}</h1>
+    <h1>{{ count }}</h1>
 
-  {{ pending ? 'Loading' : 'Loaded' }}
+    {{ pending ? 'Loading' : 'Loaded' }}
 
-  <div v-if="pending">Loading...</div>
-  <pre v-else>
+    <div v-if="pending">Loading...</div>
+    <pre v-else>
   <code>
 {{ JSON.stringify(data, null, 2) }}
   </code>
   </pre>
+  </div>
 </template>
 
 <script lang="ts" setup>
